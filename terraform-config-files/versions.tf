bacdf04617387669478428f5fba9d90bd0117152
Yee Mon <40330106+yeemon4398@users.noreply.github.com>
2024-03-09 14:34:30 +0630

    Upload versions.tf

---
+++ terraform-config-files/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["/home/vagrant/.aws/config"]
+  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+  profile                  = "bca-master"
+  region                   = "ap-southeast-1"
+}
